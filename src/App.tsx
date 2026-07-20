7b4afea4f1b8839f5483bc7bf4b9fc3783adec27
allwyn46 <allwyn153@gmail.com>
2026-07-20 12:19:37 +0530

    completed the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import "./App.css";
+import Navbar from "./componets/Navbar";
 
 function App() {
   return (
     <>
-      <div>
-        <h1 className="text-center text-7xl font-jakarta font-semibold">
-          Stay ahead of the curve with our digital marketing insights and
-          strategies
-        </h1>
-      </div>
+      <Navbar />
     </>
   );
 }
--- src/componets/Navbar.tsx
+++ src/componets/Navbar.tsx
@@ -1,18 +1,53 @@
-import logo from '../assets/logo.png'
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <header>
-        <nav className='flex justify-between items-center py-4 px-8 text-gray-800'>
-            <div>
+    <header className="container">
+      <nav className="flex justify-between items-center py-4 px-8 text-gray-800">
+        <div className="flex justify-between items-center space-x-24">
+          <img src={logo} alt="Logo" className="h-8" />
+          <ul className="flex space-x-8">
+            <li className="mr-8 font-semibold text-[14px] font-jakarta cursor-pointer">
+              Service
+            </li>
+            <li className="mr-8 font-semibold text-[14px] font-jakarta cursor-pointer">
+              Agency
+            </li>
+            <li className="mr-8 font-semibold text-[14px] font-jakarta cursor-pointer">
+              Case Study
+            </li>
+            <li className="mr-8 font-semibold text-[14px] font-jakarta cursor-pointer">
+              Resources
+            </li>
+            <li className="mr-8 font-semibold text-[14px] font-jakarta cursor-pointer">
+              Contact
+            </li>
+          </ul>
+        </div>
+        <div className="flex justify-between items-center space-x-4">
+          <button className="btn">Get Started</button>
 
-            </div>
-            <div>
-
-            </div>
-        </nav>
+          <button className="button">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-bell-icon lucide-bell svgIcon"
+            >
+              <path d="M10.268 21a2 2 0 0 0 3.464 0" />
+              <path d="M3.262 15.326A1 1 0 0 0 4 17h16a1 1 0 0 0 .74-1.673C19.41 13.956 18 12.499 18 8A6 6 0 0 0 6 8c0 4.499-1.411 5.956-2.738 7.326" />
+            </svg>
+          </button>
+        </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

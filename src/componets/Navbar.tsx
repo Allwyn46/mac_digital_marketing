52a982f145471edb098a5640736aa8dee9f4e05d
allwyn46 <allwyn153@gmail.com>
2026-07-20 11:54:55 +0530

    Added fonts

---
+++ src/componets/Navbar.tsx
@@ -0,0 +1,18 @@
+import logo from '../assets/logo.png'
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav className='flex justify-between items-center py-4 px-8 text-gray-800'>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
